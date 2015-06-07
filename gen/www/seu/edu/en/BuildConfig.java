acb6636458f0142831544b6eb338e8553ac8f5c8
yuanmouren1hao <342834599@qq.com>
2015-06-07 22:28:12 +0800

    init project

---
+++ gen/www/seu/edu/en/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package www.seu.edu.en;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/www/seu/edu/en/SEU_WlanActivity.java
@@ -0,0 +1,13 @@
+package www.seu.edu.en;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SEU_WlanActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
