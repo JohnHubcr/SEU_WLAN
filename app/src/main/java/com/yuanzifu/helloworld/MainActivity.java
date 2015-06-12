37d863d1283124b79a3eb305bd37ae2ff32df7b5
yuanmouren1hao <342834599@qq.com>
2015-06-12 17:32:01 +0800

    update MainActivity.java

--- app/src/main/java/com/yuanzifu/helloworld/MainActivity.java
+++ app/src/main/java/com/yuanzifu/helloworld/MainActivity.java
@@ -2,8 +2,11 @@ package com.yuanzifu.helloworld;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,7 +16,10 @@ import android.widget.Gallery;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.apache.http.HttpResponse;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -30,8 +36,38 @@ public class MainActivity extends ActionBarActivity {
     EditText name_edittext = null;
     EditText passwort_edittext = null;
     TextView show_textvied = null;
+    final static String TAG = "seu";
+    private static final int COMPLETED = 0;
+    private static final int BEGIN = 1;
 
     Util util = new Util();
+
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == COMPLETED) {
+                String result = (String) msg.obj;
+                String new_result = StringEscapeUtils.unescapeJava(result);
+                result = UnicodeUtil.decodeUnicode(new_result);
+
+                try {
+                    JSONObject jo = new JSONObject(result);
+                    if (jo.has("success")) {
+                        show_textvied.setText("success info:" + result);
+                    } else {
+                        show_textvied.setText("error info:" + result);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            } else if (msg.what == BEGIN) {
+                show_textvied.setText("begining");
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,24 +84,19 @@ public class MainActivity extends ActionBarActivity {
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                show_textvied.setText("begining");
+                Log.v(TAG, "begining");
+
                 /*get name and password*/
                 String login_name = name_edittext.getText().toString();
                 String login_password = passwort_edittext.getText().toString();
-                //Toast.makeText(getApplicationContext(), login_name+"-"+login_password, Toast.LENGTH_LONG).show();
+                Log.v(TAG, login_name + "-" + login_password);
 
-                /*sent post to server*/
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("username", "220131440");
-                params.put("password", "574423");
-                String result = HttpUtils.sendPostMessage(params, "utf-8");
-                System.out.println("System-test");
-                System.out.println("System-" + result);
-                HttpUtils.main(null);
-
-                show_textvied.setText(result);
-
-                showImage();
-
+                /*new Thread to send post*/
+                WorkThread doPostThread = new WorkThread();
+                doPostThread.setUsername(login_name);
+                doPostThread.setPassword(login_password);
+                doPostThread.start();
 
             }
         });
@@ -130,5 +161,45 @@ public class MainActivity extends ActionBarActivity {
             }
         }
 
+    }
+
+    //工作线程
+    private class WorkThread extends Thread {
+
+        private String username;
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        @Override
+        public void run() {
+            /*sent post to server*/
+            Map<String, String> params = new HashMap<String, String>();
+            params.put("username", username);
+            params.put("password", password);
+            String result = HttpUtils.sendPostMessage(params, "utf-8");
+            Log.v(TAG, "System-test-logcat");
+            Log.v(TAG, result);
+
+            //处理完成后给handler发送消息
+            Message msg = new Message();
+            msg.what = COMPLETED;
+            msg.obj = result;
+            handler.sendMessage(msg);
+        }
     }
 }
